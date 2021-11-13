20a5cda314d0c412ab988663261c103177f8c0e8
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-13 14:32:26 +0700

    Update dev.py

--- NaoRobot/modules/dev.py
+++ NaoRobot/modules/dev.py
@@ -40,11 +40,11 @@ def leave(update: Update, context: CallbackContext):
             bot.leave_chat(int(chat_id))
         except TelegramError:
             update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho)."
+                "**Pffftttt, I Could Not Leave That Group(Dunno Why Tho)**."
             )
             return
         with suppress(Unauthorized):
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("**Beep boop, I left that soup!.**")
     else:
         update.effective_message.reply_text("Send a valid chat ID")
 
